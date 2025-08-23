dfda4c86c8b56f528d14fcbf6313ceb0c7b5f4d7
Tinswan <tinswan@redriot.net>
2025-08-23 14:28:52 -0400

    various fixes

--- config/default.nix
+++ config/default.nix
@@ -5,11 +5,14 @@ _: {
     ./file_types.nix
     ./keymaps.nix
     ./settings.nix
+    ./keymaps.nix
+    ./settings.nix
 
     # Themes
     ./plugins/themes
 
     # Completion
+    ./plugins/cmp/autopairs.nix
     ./plugins/cmp/autopairs.nix
     ./plugins/cmp/cmp.nix
     ./plugins/cmp/lspkind.nix
@@ -20,8 +23,13 @@ _: {
 
     # Editor plugins and configurations
     ./plugins/editor/hex.nix
+    ./plugins/editor/hex.nix
     ./plugins/editor/illuminate.nix
     ./plugins/editor/indent-blankline.nix
+    ./plugins/editor/iron.nix
+    ./plugins/editor/navic.nix
+    ./plugins/editor/neo-tree.nix
+    ./plugins/editor/nix-develop.nix
     ./plugins/editor/iron.nix
     ./plugins/editor/navic.nix
     ./plugins/editor/neo-tree.nix
@@ -29,11 +37,17 @@ _: {
     ./plugins/editor/todo-comments.nix
     ./plugins/editor/treesitter.nix
     ./plugins/editor/undotree.nix
+    ./plugins/editor/treesitter.nix
+    ./plugins/editor/undotree.nix
 
     # UI plugins
     ./plugins/ui/bufferline.nix
     ./plugins/ui/harpoon.nix
+    ./plugins/ui/harpoon.nix
     ./plugins/ui/lualine.nix
+    ./plugins/ui/neorg.nix
+    ./plugins/ui/precognition.nix
+    # ./plugins/ui/qmk.nix
     ./plugins/ui/neorg.nix
     ./plugins/ui/precognition.nix
     # ./plugins/ui/qmk.nix
@@ -43,24 +57,33 @@ _: {
     ./plugins/lsp/conform.nix
     ./plugins/lsp/fidget.nix
     ./plugins/lsp/lsp.nix
+    ./plugins/lsp/lsp.nix
 
     # Git
     ./plugins/git/gitsigns.nix
     ./plugins/git/lazygit.nix
+    ./plugins/git/lazygit.nix
 
     # Utils
     ./plugins/utils/auto-save.nix
+    ./plugins/utils/auto-save.nix
     ./plugins/utils/extra_plugins.nix
-    #./plugins/utils/hardtime.nix
+    ./plugins/utils/hardtime.nix
     ./plugins/utils/markdown-preview.nix
     ./plugins/utils/mini.nix
+    ./plugins/utils/mini.nix
     ./plugins/utils/obsidian.nix
+    ./plugins/utils/telescope.nix
+    ./plugins/utils/timerly.nix
     ./plugins/utils/telescope.nix
     ./plugins/utils/timerly.nix
     ./plugins/utils/toggleterm.nix
     ./plugins/utils/trouble.nix
     ./plugins/utils/wakatime.nix
+    ./plugins/utils/trouble.nix
+    ./plugins/utils/wakatime.nix
     ./plugins/utils/web-devicons.nix
+    ./plugins/utils/whichkey.nix
     ./plugins/utils/whichkey.nix
   ];
 }
